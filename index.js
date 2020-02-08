b274914ffd8d8bbf9dbbac88297e7d05a0862914
Gilberto Almeida <almeidanetogilberto@gmail.com>
2020-02-08 09:34:11 +0100

    fixed typo

--- index.js
+++ index.js
@@ -62,7 +62,7 @@ io.on("connection", socket => {
 
 // Server static assets if inproduction
 //For building both backend and front end to heroku.
-if (process.nextTick.NODE_ENV === "production") {
+if (process.env.NODE_ENV === "production") {
   //Set static folder
   app.use(express.static("client/build"));
 
